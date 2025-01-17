a0f325fa93311ea79a17a08f3190c5e84194b13c
zamzami-bekasi <rahmat.zamzami@gmail.com>
2025-01-17 09:54:46 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# PAGES
-PAGES
+# Private Profile of Kirito-kun
+
+Live Page [Kirito-kun](https://kiritokun.pages.dev/)
+
+* Skill
+* Experiences
+* Hobbies
+* Contact
+
+> Thanks to [OSSPhilippines](https://github.com/OSSPhilippines)
+
+> License under the [MIT License](https://github.com/OSSPhilippines/freefolio/blob/main/LICENSE)
---
+++ assets/js/config.js
@@ -0,0 +1,87 @@
+window.PorfolioConfig = {
+  name: 'R. Z. Z.', 
+  jobTitle: 'Network, System and Security Pinter',
+  jobDescription: 'Don\'t tell anybody about yourself',
+  skills: [
+    {
+      name: 'Network Administration & Management',
+      description: 'Expertise in configuring and managing network infrastructures, including routing, switching, VPNs, NAT, ACLs, and overall network administration. Proficient in handling complex network setups and troubleshooting with advanced tools and protocols.',
+      devices: 'Cisco, Juniper, Ruijie, Mikrotik, TP-Link, Unifi, Frr',
+      certifications: 'CCNA, CCNP, MTCNA, MTCTCE, RCNA'
+    },
+    {
+      name: 'Wireless Network Management',
+      description: 'Proficient in deploying and managing wireless networks with a focus on optimizing coverage, performance, and security. Expertise in configuring and troubleshooting access points, wireless controllers, and network management systems. Includes experience with site surveys, RF planning, and advanced wireless features such as guest networking, VLANs, and roaming.',
+      devices: 'Aironet, Ruijie, Aruba, Ruckus, Unifi, TP-Link, Mikrotik',
+      certifications: 'RCWA'
+    },
+    {
+      name: 'Server and Virtualization Management',
+      description: 'Comprehensive management of server environments, including configuration, optimization, and security. Proficient in virtualization technologies, server hardening, and performance tuning. Experience with a wide range of server management tasks such as provisioning, monitoring, backups, and disaster recovery. Skilled in managing both physical and virtual servers, as well as utilizing management platforms and tools to streamline operations.',
+      devices: 'HP, Dell, IBM, Lenovo, Sun Fire - Proxmox, VMware, OpenStack',
+      certifications: '[Target Plan: RHCSA, RHCE, VCA, VCP, COA]'
+    },
+    {
+      name: 'Containerization & Orchestration',
+      description: 'Expert in deploying, managing, and scaling containerized applications using containerization and orchestration technologies. Proficient in Docker for container creation and management, Kubernetes for container orchestration and cluster management, and other tools for CI/CD and service discovery. Skilled in designing and implementing microservices architectures, managing container registries, and ensuring high availability and scalability of containerized applications.',
+      devices: 'Docker, Kubernetes, OpenShift, Docker Swarm, Helm, Prometheus, Grafana',
+      certifications: '[Target Plan: CKA]'
+    },
+    {
+      name: 'Cyber Security Management',
+      description: 'Skilled in implementing and managing comprehensive cybersecurity strategies to protect organizational assets from threats and vulnerabilities. Proficient in areas such as threat detection and response, risk assessment, vulnerability management, incident response, and security policy development. Experienced with various security tools and practices including firewalls, intrusion detection/prevention systems (IDS/IPS), encryption, endpoint protection, and security information and event management (SIEM). Adept at ensuring compliance with industry standards and regulations, and conducting security audits and penetration testing.',
+      devices: 'Firewalls (Fortigate, Paloalto, pfSense, Arista), IDS/IPS, SIEM (Alienvault, Wazuh, Logrhythm, Stellar), Endpoint Protection (CrowdStrike), VPN, Encryption Tools (OpenSSL), Network Scanners (Nessus, OpenVAS), Vulnerability Assessment (Burpsuite)',
+      certifications: 'CEH, [Target Plan: ECIH, CHFI]'
+    }
+  ],
+  works: [
+    {
+      name: 'PT. AUO',
+      period: '2016 - 2024',
+      description : 'Network, System and Security Engineer'
+    },
+    {
+      name: 'PT. IUS',
+      period: '2015-07 to 2015-12',
+      description : 'Network Engineer & Team Coordinator'
+    },
+    {
+      name: 'PT. PIP',
+      period: '2015-01 to 2015-06',
+      description : 'IT Support & Training Assistant'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Hiking & Survival',
+      description: 'Mt. Evrest (July 2000), Mt. Gede via Cilember (December 2021), Mt. Gede via Bekasi (June 2014)'
+    },
+    {
+      name: 'Swimming, Tubing & Rafting',
+      description: 'Tubing Ciwidey (May 2023), Rafting Riverside (June 2024)'
+    },
+    {
+      name: 'Gaming',
+      description: 'Minecraft, Dota & Arisan'
+    }
+  ],
+  contacts: {
+    email: 'kirito-kun@gmail.com',
+    mobileNo: '+62012345678',
+    phoneNo: '+21 123 456',
+    links: [
+      {
+        name: 'GitHub',
+        link: ''
+      },
+      {
+        name: 'Blogspot',
+        link: ''
+      },
+      {
+        name: 'Telegram',
+        link: ''
+      }
+    ]
+  }
+};
